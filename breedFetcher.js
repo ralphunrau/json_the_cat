b21f7b41440f8a5d4e0433f60fff8e6b87de4303
Ralph Unrau <xralphunrau@gmail.com>
2022-03-17 23:37:16 +0000

    cat breed testing

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,16 @@
 const request = require('request');
-const args = process.argv.slice(2);
-const file = `https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`;
 
-request(file, (error, response, body) => {
-  if (error) console.log(error);
-  else if (response) {
-    console.log('Response Code:', response);
-  }
-  let data = JSON.parse(body);
-  if (data[0] === undefined) {
-    console.log('This breed does not exist.');
-  } else {
-    console.log(data[0].description);
-  }
-});
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (body === '[]') error = 'That cat name does not exist';
+
+    if (error) {
+      callback(error, null);
+    } else if (body) {
+      const bodyObj = JSON.parse(body);
+      callback(null, bodyObj[0].description);
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
---
+++ test/breedFetcherTest.js
@@ -0,0 +1,24 @@
+const { fetchBreedDescription } = require('../breedFetcher');
+const { assert } = require('chai');
+
+describe('fetchBreedDescription', () => {
+  it('returns a string description for a valid breed, via callback', (done) => {
+    fetchBreedDescription('Siberian', (err, desc) => {
+      assert.equal(err, null);
+
+      const expectedDesc = "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.";
+
+      // compare returned description
+      assert.equal(expectedDesc, desc.trim());
+
+      done();
+    });
+  });
+  it('returns the first argument for callback to be set, and desc to be null', (done) => {
+    fetchBreedDescription('notABreed', (err, desc) => {
+      assert.equal(null, desc);
+      assert.equal(err, 'That cat name does not exist');
+      done();
+    });
+  });
+});
